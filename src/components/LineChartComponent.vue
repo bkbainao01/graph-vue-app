acb98076d1b280faf4205ec5ac076563b9638372
Sirimanee Pimkaew <sirimanee@imaxxsol.com>
2025-02-20 09:29:36 +0700

    update graph

--- src/components/LineChartComponent.vue
+++ src/components/LineChartComponent.vue
@@ -1,21 +1,30 @@
 <script setup>
-import { onMounted,ref,defineEmits  } from 'vue';
+import { onMounted, ref, defineEmits, watch } from 'vue';
 import { Chart } from 'chart.js';
+import { storeToRefs } from "pinia";
+import { useRefStore } from '../stores/refs';
+
+const refStore = useRefStore()
+const { refChart1, refChart2, refChart3 } = storeToRefs(refStore);
 
 const props = defineProps({
     config: Object,
     hover: Function,
     chartId: String,
-    chartRef: Object
 });
-const emit = defineEmits(['updateRef']);
-const chartRef = ref(null)
+const emit = defineEmits(['hoverOnMouseMove']);
+const chartRef = ref(null);
 
 onMounted(() => {
-    emit('update-ref', chartRef.value);
     const chart = new Chart(chartRef.value, props.config);
-    chartRef.value.onmousemove = (event) => props.hover(event, chart);
+    refStore.updateRefChart(chartRef.value, props.chartId)
+    chartRef.value.onmousemove = (event) => emit('hoverOnMouseMove', event, chart);
 });
+
+watch(chartRef, (value) => {
+    refStore.updateRefChart(value, props.chartId)
+}, { deep: true })
+
 </script>
 <template>
     <canvas :id="chartId" ref="chartRef" style="margin-left: auto; margin-right: auto; width: 80%; height: 600px;" />
